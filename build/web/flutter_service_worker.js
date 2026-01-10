fa0e20228ddb2309f9af25d7d8dcbde441927a6c
Shahidul Farid <akunsembaranganajah@gmail.com>
2026-01-10 14:57:25 +0700

    fix build web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f9aa45020ab403705caf1180601d32e6
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "eed79a908b3186943d23b774ff7d9524",
+"flutter_bootstrap.js": "765e992e7138edbdd2a732534f863a8e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
